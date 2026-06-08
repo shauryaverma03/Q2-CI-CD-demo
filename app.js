8984bb0b5500f7f0fd5dff29c0f69869c4620efd
shauryaverma03 <shauryaverma036@gmail.com>
2026-06-08 13:10:11 +0530

    updated the app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const http = require('http')
+const express = require('express');
+const app = express();
+const port = 3000;
 
-http.createServer((req,res) => res.end('CI/CD Working!')).listen(3000)
+app.get('/', (req, res) => {
+  res.send('<h1>CI/CD Pipeline is working fine!</h1>');
+});
+
+app.get('/shaurya', (req, res) => {
+    res.send('<h1>Hello I am shaurya This is my DevOps End term Execution!</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
